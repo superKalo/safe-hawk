b2c3b863c3cbd58b728c51313de005fd6afb73e1
Timothy Z. <timothy@stremio.com>
2024-10-17 23:36:27 +0300

    refactor: Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,51 +1,27 @@
 import classNames from 'classnames'
 import styles from './Input.module.scss'
+import { InputHTMLAttributes } from 'react'
 
-type Props = {
-    className?: string
-    type?: string
+type Props = InputHTMLAttributes<HTMLInputElement> & {
     name: string
     label?: string
     small?: boolean
-    readOnly?: boolean
-    required?: boolean
-    min?: number
-    max?: number
-    defaultValue?: string | number
-    disabled?: boolean
-    placeholder?: string
 }
 
 const Input = ({
-    className,
-    type,
     name,
+    className,
     label,
     small,
-    readOnly,
-    min,
-    max,
-    required,
-    defaultValue,
-    disabled,
-    placeholder,
     ...props
 }: Props) => {
     return (
         <div className={classNames(styles.input, className, { [styles.small]: small })}>
             {label ? <label htmlFor={name}>{label}</label> : null}
             <input
-                type={type ?? 'text'}
                 id={name}
                 name={name}
                 {...props}
-                readOnly={readOnly}
-                min={min}
-                max={max}
-                required={required}
-                defaultValue={defaultValue}
-                disabled={disabled}
-                placeholder={placeholder}
             />
         </div>
     )
