297d6d75b191d0628208d2308826e32256e92229
Petromir Petrov <petromirdev@gmail.com>
2024-10-17 15:14:39 +0300

    add: radix-ui themes css file

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Theme } from '@radix-ui/themes'
 import './styles/global.scss'
+import '@radix-ui/themes/styles.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
