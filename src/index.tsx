6f2c6ae34b9abcf7061a46ad610355aeb1801fdd
Petromir Petrov <petromirdev@gmail.com>
2024-10-17 12:11:16 +0300

    add: theme colors, mixins and styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.scss'
+import { Theme } from '@radix-ui/themes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>
 )
