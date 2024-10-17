3f92e376932ad6e8358032298fc8515790425f6d
Timothy Z. <timothy@stremio.com>
2024-10-17 20:49:11 +0300

    feat: init wagmi

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import { Theme } from '@radix-ui/themes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './wagmiConfig'
 import '@radix-ui/themes/styles.css'
 import '@/styles/global.scss'
+import App from './App'
+
+const queryClient = new QueryClient()
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
     <React.StrictMode>
-        <Theme>
-            <App />
-        </Theme>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <Theme>
+                    <App />
+                </Theme>
+            </QueryClientProvider>
+        </WagmiProvider>
     </React.StrictMode>
 )
