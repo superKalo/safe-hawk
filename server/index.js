166cd170adf81deecd675658da42eed6a442b8f9
Petromir Petrov <petromirdev@gmail.com>
2024-10-17 15:14:29 +0300

    add(wip): backend

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import sendMail from './services/mail.js'
+import scheduleHealthScoreCron from './cron/healthscore.js'
+
+// Load environment variables from .env file
+dotenv.config()
+
+const app = express()
+const PORT = 5000
+
+scheduleHealthScoreCron()
+
+app.post('/send-mail', async (req, res) => {
+    const { address } = req.body || {}
+
+    if (!address) return res.status(400).send('Invalid request')
+    try {
+        await sendMail(address)
+
+        return res.send('success')
+    } catch (e) {
+        res.send('error' + e)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`HealthHawk is running on port ${PORT}`)
+})
