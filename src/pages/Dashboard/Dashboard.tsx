6099ef6b555c0a7ffeacbb01b9a2453d012c3b22
Timothy Z. <timothy@stremio.com>
2024-10-18 16:51:46 +0300

    fat: init data on dashboard (2)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,33 +1,34 @@
-
-//      {/* Display AAVE Data */}
-//      <div className={styles.aaveData}>
-//      {isConnecting || isReconnecting ? (
-//          <p>Connecting...</p>
-//      ) : !accountAddress ? (
-//          <p>
-//              Please connect your wallet or input an address to view your AAVE
-//              account data.
-//          </p>
-//      ) : error ? (
-//          <p>Error: {error.message}</p>
-//      ) : isLoading ? (
-//          <p>Loading AAVE data...</p>
-//      ) : aaveData ? (
-//          <>
-//              <h4>{`Address: ${accountAddress}`}</h4>
-//              <p>{`totalCollateralETH: ${aaveData.totalCollateralETH}`}</p>
-//              <p>{`totalDebtETH: ${aaveData.totalDebtETH}`}</p>
-//              <p>{`availableBorrowsETH: ${aaveData.availableBorrowsETH}`}</p>
-//              <p>{`currentLiquidationThreshold: ${aaveData.currentLiquidationThreshold}`}</p>
-//              <p>{`ltv: ${aaveData.ltv}`}</p>
-//              <p>{`healthFactor: ${aaveData.healthFactor}`}</p>
-//          </>
-//      ) : null}
-//  </div>
-
+import { useAAVEDataProvider } from '@/context';
+import styles from './Dashboard.module.scss';
 const Dashboard = () => {
+	const { address, isConnecting, isReconnecting, isLoading, aaveData, error } = useAAVEDataProvider();
 	return (
-		<div>Dashboard</div>
+		<div>
+			<div className={styles.aaveData}>
+				{isConnecting || isReconnecting ? (
+					<p>Connecting...</p>
+				) : !address ? (
+					<p>
+						Please connect your wallet or input an address to view your AAVE
+						account data.
+					</p>
+				) : error ? (
+					<p>Error: {error.message}</p>
+				) : isLoading ? (
+					<p>Loading AAVE data...</p>
+				) : aaveData ? (
+					<>
+						<h4>{`Address: ${address}`}</h4>
+						<p>{`totalCollateralETH: ${aaveData.totalCollateralETH}`}</p>
+						<p>{`totalDebtETH: ${aaveData.totalDebtETH}`}</p>
+						<p>{`availableBorrowsETH: ${aaveData.availableBorrowsETH}`}</p>
+						<p>{`currentLiquidationThreshold: ${aaveData.currentLiquidationThreshold}`}</p>
+						<p>{`ltv: ${aaveData.ltv}`}</p>
+						<p>{`healthFactor: ${aaveData.healthFactor}`}</p>
+					</>
+				) : null}
+			</div>
+		</div>
 	)
 }
 
