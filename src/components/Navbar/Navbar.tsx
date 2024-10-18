f4ebc00917bc42a7f765f19c413e4547a9ecc79e
Kaloyan Kosev <superkalo@devlabs.bg>
2024-10-18 21:14:39 +0300

    Add: upon successful connection, redirect

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,19 @@ import { MainLogo } from '@/assets/images';
 import { CustomConnectWalletButton } from '@/components';
 import styles from './Navbar.module.scss';
 import { useNavigate } from 'react-router';
+import { useAccount } from 'wagmi';
+import { useEffect } from 'react';
+import usePrevious from '@/common/usePrevious';
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const { isConnected } = useAccount();
+    const prevIsConnected = usePrevious(isConnected)
+
+    useEffect(() => {
+        const didJustGotConnected = !prevIsConnected && isConnected === true;
+        if (didJustGotConnected) navigate('/dashboard');
+    })
 
     const onClick = () => {
         navigate('/');
