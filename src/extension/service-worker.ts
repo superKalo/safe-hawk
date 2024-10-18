1abf7bc8302ccc75bab0300119914307aa0a261b
sonytooo <simeonpetkov11999977@gmail.com>
2024-10-18 15:11:25 +0300

    add working extension with the prod build command

---
+++ src/extension/service-worker.ts
@@ -0,0 +1,50 @@
+import { JsonRpcProvider } from 'ethers'
+import { getAAVEUserContractDataFormatted } from '../../src/libs/getAAVEContractDataFormatted'
+
+async function runHeartbeat() {
+    await chrome.storage.local.set({ 'last-heartbeat': new Date().getTime() })
+}
+
+async function keepSwAlive() {
+    // Run the heartbeat once at service worker startup.
+    runHeartbeat().then(() => {
+        // Then again every 20 seconds.
+        setInterval(runHeartbeat, 20 * 1000)
+    })
+}
+
+keepSwAlive()
+
+const runDataUpdate = async () => {
+    const networkRPCURLs = {
+        ethereum: 'https://eth-mainnet.g.alchemy.com/v2/XqYEVNd8CVHFp_U2sLXA-gWuxq58d4BB',
+        avalanche: 'https://api.avax.network/ext/bc/C/rpc',
+        polygon: 'https://polygon-rpc.com',
+        optimism: 'https://mainnet.optimism.io',
+        arbitrum: 'https://arb1.arbitrum.io/rpc'
+    }
+    const provider = new JsonRpcProvider(networkRPCURLs.ethereum)
+
+    const update = async () => {
+        const data = await getAAVEUserContractDataFormatted(
+            '0x4F3c11ac6f552E36211661d161360e4A7677C683',
+            provider
+        )
+
+        if (Number(data.healthFactor) < 2.5) {
+            await chrome.notifications.create(Math.random().toString(), {
+                type: 'basic',
+                iconUrl: chrome.runtime.getURL('assets/icon@96.png'),
+                title: 'Health Factor Alert',
+                message: `You are at risk. You health factor is ${data.healthFactor}`
+            })
+        }
+        setTimeout(() => {
+            update()
+        }, 15000)
+    }
+
+    update()
+}
+
+runDataUpdate()
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import webpack from 'webpack'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
-    mode: 'development',
+    mode: 'production',
+    target: 'web',
     entry: {
-        background: './extension/service-worker.ts',
-        popup: './extension/popup.ts'
+        background: './src/extension/service-worker.ts',
+        popup: './src/index.tsx'
     },
     output: {
         filename: '[name].js',
@@ -17,27 +19,59 @@ export default {
         clean: true
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js', '.tsx', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
     },
     module: {
         rules: [
             {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
+                test: /.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', { runtime: 'automatic' }],
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-        hot: true
+        hot: true,
+        devMiddleware: {
+            writeToDisk: true
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './extension/popup.html',
+            template: './public/index.html',
             filename: 'popup.html',
             chunks: ['popup']
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './public/manifest.json', to: 'manifest.json' },
+                {
+                    from: './src/assets/icons',
+                    to: 'assets'
+                }
+            ]
         })
     ]
 }
