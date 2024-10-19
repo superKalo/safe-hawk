cce2c7972fd3a8bb264d58bd7bfdff5b6cf9f60c
Petromir Petrov <petromirdev@gmail.com>
2024-10-19 10:53:48 +0300

    add: dashboard header

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,11 +3,21 @@ import { Page } from '@/components'
 import EmailAndExtension from './EmailAndExtension'
 import styles from './Dashboard.module.scss'
 import { CurrentLTV } from './CurrentLTV'
+import { useAAVEDataProvider } from '@/context'
 
 const Dashboard = () => {
+    const { aaveData } = useAAVEDataProvider()
+
     return (
         <Page className={styles.wrapper}>
             <div className={styles.container}>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>AAVE v3 positions</h1>
+                    <p className={styles.text}>
+                        Total Collateral: {aaveData?.totalCollateralETH || '-$'} | Total Debt:{' '}
+                        {aaveData?.totalDebtETH || '-$'}
+                    </p>
+                </div>
                 <div className={styles.content}>
                     <HealthFactor />
                     <CurrentLTV />
