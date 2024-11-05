754d61ff3c429b44e8b2ec9af3155963dab5d277
Petromir Petrov <petromirdev@gmail.com>
2024-11-05 10:06:28 +0200

    add: send-emails bash script

---
+++ bin/send-emails.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Check if today is Monday (1 = Monday, 7 = Sunday)
+if [ $(date +%u) -ne 1 ]; then
+  echo "Today is not Monday. Exiting."
+  exit 0
+fi
+# Run the yarn start-server command
+yarn start-server
