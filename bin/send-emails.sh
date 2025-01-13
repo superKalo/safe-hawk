e3951fd6d04c289dada38c923deab2bb196b9c08
Petromir Petrov <petromirdev@gmail.com>
2025-01-13 15:57:45 +0200

    return: is monday check

--- bin/send-emails.sh
+++ bin/send-emails.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Skip Monday check for now
 # Check if today is Monday (1 = Monday, 7 = Sunday)
-# if [ $(date +%u) -ne 1 ]; then
-#   echo "Today is not Monday. Exiting."
-#   exit 0
-# fi
+if [ $(date +%u) -ne 1 ]; then
+  echo "Today is not Monday. Exiting."
+  exit 0
+fi
 export NODE_OPTIONS=--max-old-space-size=512
 yarn install
 
